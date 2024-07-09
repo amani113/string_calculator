43f74ad937ef7507c417bedaac4c90911bbcba16
amani <amani.keesara@gmail.com>
2024-07-09 21:49:58 +0530

    handles multiple numbers and delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,25 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
+
+    it 'returns the sum of numbers' do
+      calculator = StringCalculator.new
+      expect(calculator.add('1,2')).to eq(3)
+    end
+
+    it 'retunrs error for non integer number' do
+      calculator = StringCalculator.new
+      expect(calculator.add('xyz')).to eq(0)
+    end
+
+    it 'raises an exception with all negative numbers listed' do
+      calculator = StringCalculator.new
+      expect { calculator.add("1,-2,-3,4") }.to raise_error("Negative numbers not allowed: -2, -3")
+    end
+
+    it 'returns the sum of numbers separated by new lines' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,11 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
+    num_array = numbers.split(/,|\n/).map(&:to_i)
+    negative_numbers = num_array.select {|n| n < 0}
+    unless negative_numbers.empty?
+      raise "Negative numbers not allowed: #{negative_numbers.join(', ')}"
+    end
+    return num_array.sum
   end
 end
